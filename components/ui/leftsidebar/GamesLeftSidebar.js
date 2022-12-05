91e7ce079eca0df2bac1f1b73837b8fb94c01daf
Jeeva Kalaiselvam <jeevak001@outlook.com>
2022-12-05 19:47:37 +0530

    Level up Removed

--- components/ui/leftsidebar/GamesLeftSidebar.js
+++ components/ui/leftsidebar/GamesLeftSidebar.js
@@ -40,7 +40,7 @@ export default function GamesLeftSidebar() {
       <GamesPageMenu />
       {/* <ProfileTrophyProgress /> */}
       <ProfileLevel />
-      <ProfileLevelUp />
+      {/* <ProfileLevelUp /> */}
       <ProfilePlatinum />
       <ProfileTrophies />
       {/* {gameId && <ProfileToGet />} */}
